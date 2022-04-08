831b227f078d0464c82f163a6b6f629aa5dd4c35
Cameron Brill <c00brill@gmail.com>
2022-04-08 13:38:17 -0400

    unskip tests

--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -9,12 +9,10 @@ class TwoFerTest < Minitest::Test
   end
 
   def test_a_name_given
-    skip
     assert_equal "One for Alice, one for me.", TwoFer.two_fer("Alice")
   end
 
   def test_another_name_given
-    skip
     assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
   end
 end
