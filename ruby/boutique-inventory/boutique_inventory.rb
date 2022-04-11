ceb18e8c694b18dc56499cd16b195f83fe4fa97f
Cameron Brill <c00brill@gmail.com>
2022-04-11 16:52:38 -0400

    finish boutique inventory exercise

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -4,23 +4,23 @@ class BoutiqueInventory
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map { |item| item[:name] }.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select { |item| item[:price] < 30 }
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item| item[:quantity_by_size].map {|_, stock| stock }.sum == 0 }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find { |item| item[:name] == name }[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.map { |item| item[:quantity_by_size].map {|_, stock| stock } }.flatten.sum
   end
 
   private
